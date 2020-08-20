bee572fab118a4ba53d9dbcb485815101060d6ab
Amsu Warner <aswb4@hotmail.com>
2020-08-19 21:27:08 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+TRAP MUSIC SIMPLE STUDIO
+
+This is a web application that allow's users to make simple trap sounds with a number of 6 pads. This web application is also mobile freindly.
---
+++ app.js
@@ -0,0 +1,34 @@
+window.addEventListener('load', () => {
+    const sounds = document.querySelectorAll(".sound");
+    const pads = document.querySelectorAll(".pads div");
+    const visual = document.querySelector('.visual');
+    const colors = [
+        "#f5af19",
+        "#f12711",
+        "#F37335",
+        "#2657eb",
+        "#0f9b0f",
+        "#4A00E0"
+    ];
+
+    // Lets get sounds going
+    pads.forEach((pad, index) => {
+        pad.addEventListener('click', function () {
+            sounds[index].currentTime = 0;
+            sounds[index].play();
+
+            createBubbles(index);
+        });
+    });
+
+    // Create a function that makes bubbles
+    const createBubbles = (index) => {
+        const bubble = document.createElement("div");
+        visual.appendChild(bubble);
+        bubble.style.backgroundColor = colors[index];
+        bubble.style.animation = "jump 1s ease";
+        bubble.addEventListener('animationend', function () {
+            visual.removeChild(this);
+        })
+    };
+});
